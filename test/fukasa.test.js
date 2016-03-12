6e4072be4ee8c8168a6a2ba62b03754dcc3d014e
niwaringo:mac <niwaringo@gmail.com>
2016-03-12 23:06:25 +0900

    use webdriverio

--- test/fukasa.test.js
+++ test/fukasa.test.js
@@ -1,5 +1,17 @@
-describe("first", function() {
-    it("test", function() {
-       assert(true);
+"use strict";
+
+const assert = require("assert");
+
+describe('app login flow', function() {
+    beforeEach(() => {
+       browser.url("/test/test.html");
+    });
+    
+    it('sets up initial variables', function() {
+        let check_dom = browser.isExisting("#dom05");
+        assert.strictEqual(check_dom, false);
+        browser.scroll(0, 2501);
+        check_dom = browser.isExisting("#dom05");
+        assert.strictEqual(check_dom, true);
     });
 });
